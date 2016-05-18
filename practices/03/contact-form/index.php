56dc46a491117e11a79f071bdee6b4fe08516ef5
tastujimiyarapi <tatsuji.miyara.pi@gmail.com>
2016-05-18 17:15:46 +0900

    送信処理と回答のCSSを追加

--- practices/03/contact-form/index.php
+++ practices/03/contact-form/index.php
@@ -1,4 +1,7 @@
 <?php
+// 設定ファイルを読み込み.
+$settings = require __DIR__ . '/../../secret-settings.php';
+
 //セッションのスタート
 session_start();
 
@@ -18,12 +21,44 @@ switch(strtolower($_SERVER["REQUEST_METHOD"])){
         //項目チェック
         validate($_POST, $messages);
 
-        //エラーメッセージがある場合
         if(!isset($messages) || count($messages) > 0){
+        	//エラーメッセージがある場合
             $css_ms = true;
         } else {
             //ここでメール送信処理を行う。
-            if(true){
+            mb_language('Japanese');
+            mb_internal_encoding('UTF-8');
+            
+            //本文作成
+            $mailtmp = <<< EOF
+
+------------------------------------------------------------
+お名前：{$_POST['name']}
+------------------------------------------------------------
+メールアドレス：{$_POST['email']}
+------------------------------------------------------------
+お電話番号：{$_POST['tel']}
+------------------------------------------------------------
+ご質問内容：
+{$_POST['contents']}
+------------------------------------------------------------
+
+EOF;
+			//Webサイト管理者宛て
+			$mailtx1 = <<< EOF
+以下のお客様からお問い合わせがありました。
+{$mailtmp}
+EOF;
+			//ユーザー宛て
+			$mailtx2 = <<< EOF
+以下の内容でお問い合わせを受け付けました。
+担当者より折り返しご連絡を差し上げますので、今しばらくお待ちください。
+{$mailtmp}
+EOF;
+
+			//メール送信処理
+            if((mb_send_mail($settings['email'], h('お問い合わせがありました'), $mailtx1, 'From: ' . mb_encode_mimeheader('テスト') . ' <no-reply@example.com>'))
+            		&& (mb_send_mail($_POST['email'], h('お問い合わせがありがとうございました'), $mailtx2, 'From: ' . mb_encode_mimeheader('テスト') . ' <no-reply@example.com>'))){
                 $css_ms = false;
                 //送信しましたのメッセージを表示する。
                 $messages[] = "メールの送信が完了しました。";
@@ -44,60 +79,92 @@ switch(strtolower($_SERVER["REQUEST_METHOD"])){
 <html>
 <head>
     <meta charset="UTF-8"/>
-    <title>test</title>
-    <style type="text/css">
-    table{
-        width:1000px;
-    }
-    table tr{
-        height:35px;
-    }
-    table .item{
-        width :110px;
-        font-size: 15px;
-        font-weight :bold ;
-    }
-    .ok{
-        color:red;
-    }
+    <title>問い合わせフォーム</title>
+        <style type="text/css">
+        table {
+            border-collapse: collapse;
+        }
+        table thead tr td.success {
+            border: 1px solid #6c6;
+            background-color: #dfd;
+            padding: 5px 10px;
+        }
+        table thead tr td.error {
+            border: 1px solid #c66;
+            background-color: #fdd;
+            padding: 5px 10px;
+            color: #f00;
+        }
+        table tbody tr th,
+        table tbody tr td {
+            border: none;
+            padding: 10px;
+        }
+        table tbody tr td {
+            width: 300px;
+        }
+        table tbody tr th {
+            text-align: left;
+            vertical-align: top;
+        }
+        table tbody tr th span {
+            color: #f00;
+        }
+        table tbody tr td input,
+        table tbody tr td textarea {
+            width: 100%;
+        }
+        table tfoot tr td {
+            text-align: right;
+        }
     </style>
 </head>
 <body>
-<p class="fm"><B>問い合わせフォーム</B></p>
+<h1>問い合わせフォーム</h1>
 <form action="index.php" method="POST">
 <table>
+    <thead>
 <?php foreach ($messages as $message){ ?>
     <tr>
-        <td class="<?php $css_ms ? ng : ok ;?>" colspan ="2" border=1 ><?= h($message); ?></td>
+        <td class="<?php if($css_ms){ echo "error";} else { echo "success";} ;?>" colspan ="2" border=1 ><?= h($message); ?></td>
     </tr>
 <?php } ?>
+    </thead>
+    <tbody>
     <tr>
-        <td class ="item">*お名前</td>
+        <th><span>*</span> お名前</th>
         <td><input type="text" name="name" value="<?= h($_POST['name']) ?>" placeholder="例）山田 太郎"/></td>
     </tr>
     <tr>
-        <td class ="item">*メールアドレス</td>
+        <th><span>*</span> メールアドレス</th>
         <td><input type="text" name="email" value="<?= h($_POST['email']) ?>" placeholder="例）email@exsample.com"/></td>
     </tr>
     <tr>
-        <td class ="item">お電話番号</td>
+        <th>お電話番号</th>
         <td><input type="text" name="tel" value="<?= h($_POST['tel']) ?>" placeholder="例）090-1234-5678"/></td>
     </tr>
     <tr>
-        <td class ="item">*ご質問内容</td>
+        <th><span>*</span> ご質問内容</th>
         <td><textarea name="contents" placeholder="ご自由にお書きください"><?= h($_POST['contents']) ?></textarea></td>
     </tr>
-    <tr>
-        <td><input type="submit" value="送信"></td>
-        <td><input type="reset" value="リセット"></td>
-    </tr>
+    </tbody>
+    <tfoot>
+        <td colspan="2">
+            <input type="hidden" name="csrf_key" value="<?=generateCsrfKey(); ?>">
+            <input type="submit" value="送信">
+            <input type="reset" value="リセット">
+        </td>
+    </tfoot>
 </table>
-<input type="hidden" name="csrf_key" value="<?=generateCsrfKey(); ?>">
 </form>
-<p></p>
 </body>
 </html>
 <?php
+//メール本文作成
+function mailText(){
+}
+
+
 //入力項目のチェックを行う。
 function validate($post, &$messages) {
     //必須チェック(名前、アドレス、電話番号、質問内容)
